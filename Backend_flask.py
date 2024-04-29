ca1be2b0e996cca122b02ac2057d3252cd0a6180
Azizbek <fattoevazizbek@gmail.com>
2024-04-29 20:16:24 +0900

    merge method_refactoring and integrate functions in the branch

--- Backend_flask.py
+++ Backend_flask.py
@@ -1,34 +1,9 @@
 from flask import Flask, request, jsonify
+import geocoding
 
 app = Flask(__name__)
 
-# Dummy data for transportation options
-transportation_options = {
-    "car": {
-        "duration": 3
-    },
-    "bus": {
-        "duration": 5
-    },
-    "train": {
-        "duration": 2
-    }
-}
-
-# Function to calculate and return route
-def return_route(transportation_type, from_city, to_city):
-    if transportation_type in transportation_options:
-        route = {
-            "from": from_city,
-            "to": to_city,
-            "transportation_type": transportation_type,
-            "duration": transportation_options[transportation_type]["duration"]
-        }
-        return jsonify(route)
-    else:
-        return jsonify({"error": "Invalid transportation type"}), 400
-
-# Endpoint for getting transportation options
+# Endpoint for getting the route data
 @app.route('/route', methods=['GET'])
 def get_route():
     # Get parameters from request
@@ -37,7 +12,7 @@ def get_route():
     to_city = request.args.get('to_city')
 
     # Call function to return route
-    return return_route(transportation_type, from_city, to_city)
+    return geocoding(transportation_type, from_city, to_city)
 
 # Homepage
 @app.route('/')
