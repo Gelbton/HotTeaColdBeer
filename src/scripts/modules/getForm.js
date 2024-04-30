841839858b31853304b5190e81e99e19ba76de00
asilbek0704 <nizamovasilbek@gmail.com>
2024-05-01 03:00:51 +0900

    backend integration

--- src/scripts/modules/getForm.js
+++ src/scripts/modules/getForm.js
@@ -4,49 +4,12 @@ import { renderData } from './renderData';
 export const getForm = async event => {
   event.preventDefault();
 
-  const form = event.currentTarget;
+  const detailsForm = document.querySelector(".details__form");
   const { from, to, vehicle } = event.target.elements;
 
   const data = await fetchData(from.value, to.value, vehicle.value);
 
-  console.log(data);
+  renderData(data);
 
-  const testData = {
-    duration: '3 hrs 20 mins',
-    from: 'Tokio, Japan',
-    to: 'Incheon, South Korea',
-    instructions: [
-      {
-        distance: 76,
-        text: 'Continue onto Lindenschmitstraße',
-        street_name: 'Lindenschmitstraße',
-      },
-      {
-        distance: 6,
-        text: 'Turn left onto Plinganserstraße and drive toward Zentrum',
-        street_destination: 'Zentrum',
-        street_name: 'Plinganserstraße',
-      },
-      {
-        distance: 442,
-        text: 'Turn right onto Radlkoferstraße',
-        street_name: 'Radlkoferstraße',
-      },
-      {
-        distance: 64,
-        text: 'Turn right onto Hans-Fischer-Straße',
-        street_name: 'Hans-Fischer-Straße',
-      },
-      {
-        distance: 1512,
-
-        text: 'Turn left onto Lindwurmstraße',
-        street_name: 'Lindwurmstraße',
-      },
-    ],
-  };
-
-  renderData(testData);
-
-  // form.reset();
+  detailsForm.reset();
 };
--- src/scripts/modules/renderData.js
+++ src/scripts/modules/renderData.js
@@ -1,19 +1,30 @@
+import { getDirectionIcon } from './getDirectionIcon';
+
 export const renderData = data => {
-  // const { duration, from, to, instructions } = data;
-  // {text, street_name, street_destination, distance} = instructions;
+  const { hr, min, origin: from, destination: to, instructions } = data;
 
   const detailsContainer = document.querySelector('.details__results'),
     detailsRoute = document.querySelector('.details__results-route'),
     detailsStart = document.querySelector('.details__results-start'),
-    detailsEnd = document.querySelector('.details__results-end');
+    detailsEnd = document.querySelector('.details__results-end'),
+    detailsTableBody = document.querySelector('.details__results-table tbody');
+
+  detailsRoute.innerHTML = '';
+  detailsStart.innerHTML = '';
+  detailsEnd.innerHTML = '';
+  detailsTableBody.innerHTML = '';
 
   detailsContainer.classList.remove('visually-hidden');
+
+  
+
+  const time = hr > 0 ? `${hr} hrs ${min} mins` : `${min} mins`;
 
   detailsRoute.insertAdjacentHTML(
     'afterbegin',
     `
       <h2>Route:</h2>
-      <p>${data.duration}</p>
+      <p>${time}</p>
     `
   );
 
@@ -21,46 +32,39 @@ export const renderData = data => {
     'afterbegin',
     `
       <span class="details__results-circle"></span>
-      <h3 class="details__results-title">${data.from}</h3>
+      <h3 class="details__results-title">${from}</h3>
     `
   );
 
-  data.instructions.forEach(item => {
-    console.log(item);
-
+  instructions.forEach(item => {
     const tr = document.createElement('tr');
 
     tr.insertAdjacentHTML(
       'afterbegin',
       `
         <td class="details__results-about">
-          <svg width="23" height="32" viewBox="0 0 23 32" fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M11.4535 0.25L21.9414 12.2019C22.1499 12.4395 22.2126 12.7679 22.1058 13.063C21.9527 13.4861 21.5004 13.7221 21.0631 13.6247L20.9626 13.5963L14.1875 11.2557V31.75H8.8125V11.2234L1.94446 13.5963C1.6801 13.6876 1.38968 13.6521 1.15821 13.5053L1.06287 13.4357C0.716108 13.1454 0.652355 12.6524 0.897839 12.2897L0.965668 12.2019L11.4535 0.25Z"
-              fill="currentColor" />
-          </svg>
+          ${getDirectionIcon(item.path).outerHTML}
 
           <div class="details__results-descr">
-            <h3>${item.text}</h3>
-            <p>${item.street_name}</p>
+            <h3>${item.path}</h3>
+            <p>${item.street}</p>
           </div>
         </td>
 
         <td class="details__results-range">
-          <p>${item.distance} km</p>
+          <p>${item.distance_km} km</p>
         </td>
       `
     );
 
-    document.querySelector(".details__results-table tbody").appendChild(tr);
+    detailsTableBody.appendChild(tr);
   });
 
   detailsEnd.insertAdjacentHTML(
     'afterbegin',
     `
       <span class="details__results-circle"></span>
-      <h3 class="details__results-title">${data.to}</h3>
+      <h3 class="details__results-title">${to}</h3>
     `
   );
 };
